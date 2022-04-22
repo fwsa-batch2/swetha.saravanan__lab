67ba9dd00402c8f6ca8128c3d2190b9a75504f01
Swetha-saravanan <swethakarthika16@gmail.com>
2022-04-22 13:49:45 +0530

    Adding Constructor chain

---
+++ Java/Conchaininclass.java
@@ -0,0 +1,25 @@
+public class Conchaininclass {
+    Conchaininclass() {
+        System.out.println("I am  No-arg Constructor in Conchaininclass");
+    }
+    Conchaininclass(int x) {
+        System.out.println("Single Parametrized Constructor");
+    }
+    Conchaininclass(String name , int x) {
+        System.out.println("Double Parametrized Constructor");
+    }}
+    class Super extends Conchaininclass {
+    Super() {
+        System.out.println("Zero arg Constructor");
+    }
+    Super(int x)  {
+        System.out.println("One Arg Constructor");
+    }
+    Super(String name , int x) {
+        System.out.println("I am here to Confuse you");
+    }
+    public static void main(String[] args) {
+        new Super(2);
+        new Super("Swetha",17);
+        new Super();
+    }}
---
+++ Java/Constructorchain.java
@@ -0,0 +1,18 @@
+public class Constructorchain {
+    Constructorchain() {
+        this(2);
+        System.out.println("I am No-arg Constructor");
+    }
+    Constructorchain(int x) {
+        this("Swetha",17);
+        System.out.println("X value is " +x);
+        System.out.println("I am Single Parametrized Constructor");
+    }
+    Constructorchain(String name , int x) {
+        System.out.println("My name is " +name);
+        System.out.println("I am Double Parametrized Constructor");
+    }
+    public static void main(String[] args) {
+        new Constructorchain();
+    }
+}
